3d2fd37542f0f0cf2c6d5bdafb6535a1f241b29b
jojosel <121195161+jojosel@users.noreply.github.com>
2022-12-22 21:29:38 -0500

    Update SVD.py

--- Method/SVD.py
+++ Method/SVD.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O
-
-
-# In[ ]:
 
 
 # Reading ratings file
@@ -21,9 +15,6 @@ users = pd.read_csv('https://github.com/Bo-Yu-Columbia/6893_final_project_2022_1
 movies = pd.read_csv('https://github.com/Bo-Yu-Columbia/6893_final_project_2022_16/blob/main/Method/movies.csv', sep='\t', encoding='latin-1', usecols=['movie_id', 'title', 'genres'])
 
 
-# In[ ]:
-
-
 movies.head()
 ratings.head()
 
@@ -32,48 +23,25 @@ num_movies = ratings.movie_id.unique().shape[0]
 print 'Number of users = ' + str(num_users) + ' | Number of movies = ' + str(num_movies)
 
 
-# In[ ]:
-
-
 Ratings = ratings.pivot(index = 'user_id', columns ='movie_id', values = 'rating').fillna(0)
 Ratings.head()
-
-
-# In[ ]:
-
 
 R = Ratings.as_matrix()
 user_ratings_mean = np.mean(R, axis = 1)
 Ratings_demeaned = R - user_ratings_mean.reshape(-1, 1)
-
-
-# In[ ]:
-
 
 #Model-Based Collaborative Filtering¶ -SVD
 
 sparsity = round(1.0 - len(ratings) / float(num_users * num_movies), 3)
 print 'The sparsity level of MovieLens1M dataset is ' +  str(sparsity * 100) + '%'
 
-
-# In[ ]:
-
-
 from scipy.sparse.linalg import svds
 U, sigma, Vt = svds(Ratings_demeaned, k = 50)
 sigma = np.diag(sigma)
 
-
-# In[ ]:
-
-
 all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
 preds = pd.DataFrame(all_user_predicted_ratings, columns = Ratings.columns)
 preds.head()
-
-
-# In[ ]:
-
 
 def recommend_movies(predictions, userID, movies, original_ratings, num_recommendations):
     
@@ -105,23 +73,13 @@ def recommend_movies(predictions, userID, movies, original_ratings, num_recommen
     return user_full, recommendations
 
 
-# In[ ]:
-
-
 already_rated, predictions = recommend_movies(preds, 1, movies, ratings, 20)
 # Top 20 movies that User 1 has rated 
 already_rated.head(20)
 
 
-# In[ ]:
-
-
 # Top 20 movies that User 1310 hopefully will enjoy
 predictions
-
-
-# In[ ]:
-
 
 # Model evaluation
 # Import libraries from Surprise package
@@ -137,25 +95,14 @@ data = Dataset.load_from_df(ratings[['user_id', 'movie_id', 'rating']], reader)
 data.split(n_folds=5)
 
 
-# In[ ]:
-
-
 # Use the SVD algorithm.
 svd = SVD()
 
 # Compute the RMSE of the SVD algorithm.
 evaluate(svd, data, measures=['RMSE'])
 
-
-# In[ ]:
-
-
 trainset = data.build_full_trainset()
 svd.train(trainset)
-
-
-# In[ ]:
-
 
 ratings[ratings['user_id'] == 1]
 svd.predict(1, 1600)
